cc9dd5b6a2b23d085fd51c58d63ac9a999876304
RomanNft <118902504+RomanNft@users.noreply.github.com>
2024-09-23 09:25:27 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-# Встановлення Docker та Docker Compose
-sudo apt install docker.io -y
-sudo snap install docker
+# Оновлення та встановлення необхідних пакетів
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Встановлення Docker
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Додавання GPG ключа та Docker репозиторію
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Оновлення та встановлення Docker
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Встановлення Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Запуск та автозапуск Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
 
 # Встановлення .NET SDK
 sudo snap install dotnet-sdk --classic
-sudo apt-get update
 sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-8.0
 
-# Встановлення postgresql-client
+# Встановлення PostgreSQL клієнта
 sudo apt-get install -y postgresql-client
 
 # Встановлення dotnet-ef
@@ -19,10 +39,13 @@ dotnet tool install --global dotnet-ef
 export PATH="$PATH:/root/.dotnet/tools"
 
 # Виведення шляху для перевірки
-echo $PATH
+echo "Поточний PATH: $PATH"
+
+# Налаштування та запуск сервісів через Docker Compose
 cd facebook-server/
 chmod +x wait-for-postgres.sh
-
-# Запуск Docker Compose для всіх сервісів
 cd ..
-docker-compose up --build -d
+#Ставим дженкінс
+bash installJenkins.sh
+# Запуск Docker Compose з побудовою сервісів
+docker-compose up --build
